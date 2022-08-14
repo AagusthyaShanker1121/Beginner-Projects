72427598ab8ca4eab70726a3f2f4dfa292d78dfa
AagusthyaShanker1121 <99526176+AagusthyaShanker1121@users.noreply.github.com>
2022-08-14 16:32:10 +0530

    Create DiamondPattern.cpp

---
+++ DiamondPattern.cpp
@@ -0,0 +1,57 @@
+// Diamond pattern
+/* Pattern formation for input = 10
+
+         *
+        ***
+       *****
+      *******
+     *********
+    ***********
+   *************
+  ***************
+ *****************
+*******************
+*******************
+ *****************
+  ***************
+   *************
+    ***********
+     *********
+      *******
+       *****
+        ***
+         *
+
+*/
+
+
+
+
+// Program for above pattern formation
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  int n;
+  cout<< "Enter the Diamond's half length :\t"
+    cin >> n;
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n - i; j++) {
+            cout << " ";
+        }
+        for (int j = 1; j <= 2 * i - 1; j++) {
+            cout << "*";
+        }
+        cout << endl;
+    }
+    for (int i = n; i>=1; i--) {
+        for (int j = 1; j <= n - i; j++) {
+            cout << " ";
+        }
+        for (int j = 1; j <= 2 * i - 1; j++) {
+            cout << "*";
+        }
+        cout << endl;
+    }
+}
