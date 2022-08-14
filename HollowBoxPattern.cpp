c024d591d3df214871d718e6ec1189e57ec0afa1
AagusthyaShanker1121 <99526176+AagusthyaShanker1121@users.noreply.github.com>
2022-08-14 16:41:28 +0530

    Update HollowBoxPattern.cpp

--- HollowBoxPattern.cpp
+++ HollowBoxPattern.cpp
@@ -24,7 +24,7 @@ using namespace std;
 
 int main() {
   int row, col;
-  cout << "Enter the number of rows and columns :\t"
+  cout << "Enter the number of rows and columns :\t" ;
     cin >> row >> col;
     for (int i = 1; i <= row; i++) {
         for (int j = 1; j <= col; j++) {
